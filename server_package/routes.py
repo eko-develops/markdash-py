6c19a8d5c37272e093da0ae8b884444f1fa37dff
eko-develops <ekodevelops@gmail.com>
2023-03-10 12:16:44 -0800

    remove controller logic

--- server_package/routes.py
+++ server_package/routes.py
@@ -1,46 +1,63 @@
-from flask import jsonify
+"""Module for handling routes with controllers."""
 
-from server_package.models import User
-from server_package import app, db
+from flask import request
+
+from server_package import app
+from server_package.controllers import UserController as UC, AdminController as AC
 
 
-"""Dummy data"""
+# Dummy Data
 promotions = [
     {"name": "Weekly Wednesday", "description": "10% off every Wednesday, every week"},
     {"name": "Sunday Funday", "description": "20% off all items"},
 ]
 
 
-@app.route("/users")
+@app.get("/users")
 def user_list():
-    """Returns a list of dictionaries representing a user"""
-    query = db.select(User).order_by(User.id)
-    users = db.session.execute(query).scalars()
-
-    user_res_list = []
-
-    for user in users:
-        user_res_list.append(
-            {
-                "id": user.id,
-                "username": user.username,
-                "email": user.email,
-                "image_file": user.image_file,
-                "password": user.password,
-            }
-        )
-    return jsonify({"users": user_res_list})
+    """Returns a list of dictionaries representing all the users."""
+    users = UC.get_all_users()
+    return {"users": users}
 
 
-@app.route("/delete/all")
+@app.get("/user/<int:id>")
+def user_get(id):
+    """Returns a single user."""
+    user = UC.get_user_by_id(id)
+
+    if user:
+        return {"user": user}
+    return {"user": {}, "message": f"No user found with ID {id}."}, 409
+
+
+@app.post("/user")
+def user_post():
+    """Adds a new user and returns the new user."""
+    new_user_data = request.get_json()
+
+    new_user = UC.create_user(new_user_data)
+
+    response = {"new_user": {}}
+    if new_user == -1:
+        response["message"] = "Username already exists."
+        return response, 409
+    if new_user == -2:
+        response["message"] = "Email already exists."
+        return response, 409
+
+    response["message"] = "New user created."
+    return {"new_user": new_user}, 201
+
+
+@app.delete("/delete/all")
 def delete_all():
     """Deletes all tables in the database"""
-    db.drop_all()
-    return jsonify({"message": "Deleted all tables in database"})
+    AC.drop_all()
+    return {"message": "Deleted all tables in database"}
 
 
-@app.route("/create/tables")
+@app.post("/create/tables")
 def create_tables():
     """Create tables from Models"""
-    db.create_all()
-    return jsonify({"message": "Created User and Promotions tables"})
+    AC.create_tables()
+    return {"message": "Created User and Promotions tables"}
