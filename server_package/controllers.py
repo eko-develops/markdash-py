366b4bcb806436a528c20a32562a9f064a1f93e7
eko-develops <ekodevelops@gmail.com>
2023-03-10 12:16:36 -0800

    create user controller

---
+++ server_package/controllers.py
@@ -0,0 +1,107 @@
+from server_package import db
+from server_package.models import User
+
+
+class AdminController:
+    """The controller used for handling admin database operations. Development only."""
+
+    @staticmethod
+    def drop_all():
+        """Drops all tables in the database."""
+        db.drop_all()
+
+    @staticmethod
+    def create_tables():
+        """Create tables based on models."""
+        db.create_all()
+
+
+class UserController:
+    """The controller used for handling the User model."""
+
+    @staticmethod
+    def get_all_users():
+        """Returns a list of all the Users."""
+        query = db.select(User).order_by(User.id)
+        users = db.session.execute(query).scalars()
+
+        user_list = []
+
+        for user in users:
+            user_list.append(
+                {
+                    "id": user.id,
+                    "username": user.username,
+                    "email": user.email,
+                    "image_file": user.image_file,
+                    "password": user.password,
+                }
+            )
+        return user_list
+
+    @staticmethod
+    def get_user_by_id(id):
+        """Returns a user by id."""
+        query = db.select(User).where(User.id == id)
+        user = db.session.execute(query).scalar()
+
+        if user:
+            return {
+                "id": user.id,
+                "username": user.username,
+                "email": user.email,
+                "image_file": user.image_file,
+            }
+        return None
+
+    @staticmethod
+    def get_user_by_username(username):
+        """Returns a user by their username."""
+        query = db.select(User).where(User.username == username)
+        user = db.session.execute(query).scalar()
+        if user:
+            return {
+                "id": user.id,
+                "username": user.username,
+                "email": user.email,
+                "image_file": user.image_file,
+            }
+        return False
+
+    @staticmethod
+    def get_user_by_email(email):
+        """Returns a user by their email."""
+        query = db.select(User).where(User.email == email)
+        user = db.session.execute(query).scalar()
+        if user:
+            return {
+                "id": user.id,
+                "username": user.username,
+                "email": user.email,
+                "image_file": user.image_file,
+            }
+        return False
+
+    @staticmethod
+    def create_user(new_user_data):
+        username = new_user_data["username"]
+        if UserController.get_user_by_username(username):
+            return -1
+
+        email = new_user_data["email"]
+        if UserController.get_user_by_email(email):
+            return -2
+
+        password = new_user_data["password"]
+
+        new_user = User(username=username, email=email, password=password)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return {
+            "id": new_user.id,
+            "username": new_user.username,
+            "email": new_user.email,
+            "image_file": new_user.image_file,
+        }
