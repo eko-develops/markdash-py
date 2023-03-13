12020349f3d069a8c6505e5d7e236de234705a25
eko-develops <ekodevelops@gmail.com>
2023-03-13 14:28:26 -0700

    enable migration

--- server_package/__init__.py
+++ server_package/__init__.py
@@ -2,12 +2,14 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 """Creating a Flask app, setting the database location, create db object"""
 app = Flask(__name__)
 CORS(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 import server_package.routes.user_routes
 import server_package.routes.admin_routes
